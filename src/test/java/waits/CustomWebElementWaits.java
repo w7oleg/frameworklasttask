b9d3c219bc221af91075e6bcf7ca8ec3960e34a3
w7oleg <w7oleg@gmail.com>
2021-11-04 14:23:51 +0300

    commit

--- src/test/java/waits/CustomWebElementWaits.java
+++ src/test/java/waits/CustomWebElementWaits.java
@@ -13,20 +13,20 @@ public class CustomWebElementWaits {
     private static long WAIT_TIMEOUT_SECONDS = 10;
 
     public static WebElement waitPresenceOfElementLocatedAndElementToBeClickable(By locator, WebDriver driver) {
-        WebElement position = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+        WebElement position = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
                 .until(ExpectedConditions.presenceOfElementLocated(locator));
-        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
                 .until(ExpectedConditions.elementToBeClickable(position));
         return position;
     }
 
     public static WebElement waitElementToBeClickable(WebElement element, WebDriver driver) {
-        return new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+        return new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
                 .until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static Boolean waitPageLoad(WebDriver driver){
-       return new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS)).withMessage("javascript didn't load")
+       return new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).withMessage("javascript didn't load")
                 .until(CustomConditions.jsLoadCompleted());
     }
 
