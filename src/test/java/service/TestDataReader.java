7fbc46394a8961fa1bac310294f2de1d1efb4bce
w7oleg <w7oleg@gmail.com>
2021-11-07 10:11:42 +0300

    commit

--- src/test/java/service/TestDataReader.java
+++ src/test/java/service/TestDataReader.java
@@ -6,6 +6,7 @@ public class TestDataReader {
     private static ResourceBundle commonResources = ResourceBundle.getBundle(System.getProperty("environment"));
 
     public static String getTestData(String key) {
+        System.out.println(commonResources.getString(key));
         return commonResources.getString(key);
     }
 }
